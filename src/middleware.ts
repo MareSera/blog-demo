93f55ef307d7fe1548fd14c08d4b25cd42f01f70
MareSera <liu_081227@foxmail.com>
2024-02-19 18:50:56 +0800

    commit

--- src/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { defineMiddleware } from "astro:middleware";
-
-export const onRequest = defineMiddleware(async (context, next) => {
-    const response = await next();
-    response.headers.set('Content-Type', 'text/html; charset=utf-8');
-
-    return response;
-});
